56ba533d452442c94f5f82a4b4fa6ff7448fd868
nerjerry <psinghal4210@gmail.com>
2015-11-01 11:13:28 +0530

    Add names to input tags

--- application/views/login.php
+++ application/views/login.php
@@ -23,9 +23,9 @@
 					<div class="signup-form"><!--sign up form-->
 						<h2>New User Signup!</h2>
 						<?php echo form_open('login/register');?>
-							<input type="text" placeholder="Name"/>
-							<input type="email" placeholder="Email Address"/>
-							<input type="password" placeholder="Password"/>
+							<input type="text" name="name" placeholder="Name"/>
+							<input type="email" name="email" placeholder="Email Address"/>
+							<input type="password" name="password" placeholder="Password"/>
 							<button type="submit" class="btn btn-default">Signup</button>
 						</form>
 					</div><!--/sign up form-->
