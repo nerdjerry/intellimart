a7c686b597453e481a933dd3a1a9a8d84f36aefb
nerjerry <psinghal4210@gmail.com>
2015-11-03 01:38:34 +0530

    BUGFIX:Create private method to check for unique value and use that function

--- application/models/user.php
+++ application/models/user.php
@@ -5,6 +5,11 @@ class User extends CI_Model{
 	public function __construct(){
 		$this->load->database();
 	}
+	private function exists($columnName,$data){
+		$this->db->where($columnName,$data);
+		$query=$this->db->get('users');
+		return $query->num_rows()>0;
+	}
 	public function register(){
 		$this->load->helper('url');
 		$inputName=$this->input->post('name');
@@ -12,14 +17,16 @@ class User extends CI_Model{
 		$data['first_name']=ucfirst($first);
 		$data['last_name']=ucfirst($last);
 		$data['email_id']=$this->input->post('email');
+		$data['password']=md5($this->input->post('password'));
 		$data['mobile_no']=$this->input->post('mobile');
-		if(($this->db->where('email_id',$data['email_id']))->num_rows>0){
+		if($this->exists('email_id',$data['email_id'])){
 			return "Email Id already registered";
 		}
-		elseif(($this->db->where('mobile_no',$data['mobile_no']))->num_rows>0){
+		elseif($this->exists('mobile_no',$data['mobile_no'])){
 			return "Mobile number already registered";
+		}else{
+			return $this->db->insert('users',$data);
 		}
-		$data['password']=md5($this->input->post('password'));
-		return $this->db->insert('users',$data);
+
 	}
 }
