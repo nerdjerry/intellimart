cea95dcb613cde61fd9736b43fc4e67278fa2d3b
nerjerry <psinghal4210@gmail.com>
2015-11-03 00:18:53 +0530

    Create model for user and add register method

---
+++ application/models/user.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class User extends CI_Model{
+	public function __construct(){
+		$this->load->database();
+	}
+	public function register(){
+		$this->load->helper('url');
+		$inputName=$this->input->post('name');
+		list($first,$last)=explode(" ",$inputName);
+		$data['first_name']=ucfirst($first);
+		$data['last_name']=ucfirst($last);
+		$data['email_id']=$this->input->post('email');
+		$data['mobile_no']=$this->input->post('mobile');
+		if($this->db->where('email_id',$data['email_id'])){
+			return "Email Id already registered";
+		}
+		if else($this->db->where('mobile_no',$data['mobile_no'])){
+			return "Mobile number already registered";
+		}
+		$data['password']=md5($this->input->post('password'));
+		return $this->db->insert('users',$data);
+	}
+}
