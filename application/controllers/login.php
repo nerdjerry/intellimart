83c38cb4f5647dc40e0a4b985979c6da7b277af6
nerjerry <psinghal4210@gmail.com>
2015-11-03 02:12:11 +0530

    Add registration

---
+++ application/controllers/login.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Login extends CI_Controller{
+
+	public function register(){
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules('name','Name','required');
+		$this->form_validation->set_rules('email','Email','required|valid_email');
+		$this->form_validation->set_rules('password','Password','required');
+		if($this->form_validation->run() === FALSE){
+			$errorMessage="Missing fields";
+			$message="Please enter all the fields";
+			show_error($message,0,$errorMessage);
+		}
+		else{
+			$this->load->model('user');
+			if($errorMessage=$this->user->register()){
+				$message = "Please refill for with apt details";
+				show_error($message,0,$errorMessage);
+			}
+			//TODO:create session and login user.
+
+		}
+	}
+}
