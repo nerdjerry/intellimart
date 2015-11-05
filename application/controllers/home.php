2f5994f423b099654b3968fc0e025eab83c4ee59
nerjerry <psinghal4210@gmail.com>
2015-11-06 03:38:22 +0530

    Implement logout

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -9,4 +9,10 @@ class Home extends CI_Controller{
 		$this->load->helper('form');
 		$this->load->view('login.php');
 	}
+	public function logout(){
+		$array=array('id','name','access','logged');
+		$this->session->unset_userdata($array);
+		$this->session->sess_destroy();
+		redirect();//Go to homepage
+	}
 }
