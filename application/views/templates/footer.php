8ea843f7c7ef74b7a9b6fffd3aec2d8e3aeee6f0
nerjerry <psinghal4210@gmail.com>
2015-11-11 01:56:35 +0530

    change about section in  footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -116,13 +116,10 @@
 					</div>-->
 					<div class="col-sm-2">
 						<div class="single-widget">
-							<h2>About Shopper</h2>
+							<h2>About Intellimart</h2>
 							<ul class="nav nav-pills nav-stacked">
 								<li><a href="#">Company Information</a></li>
-								<li><a href="#">Careers</a></li>
-								<li><a href="#">Store Location</a></li>
-								<li><a href="#">Affillate Program</a></li>
-								<li><a href="#">Copyright</a></li>
+								<li><a href="#">Team</a></li>
 							</ul>
 						</div>
 					</div>
