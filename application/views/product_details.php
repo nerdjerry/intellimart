1f7f899bab408044f8e881d0d2324e57b8d8b9be
nerjerry <psinghal4210@gmail.com>
2015-11-17 17:11:15 +0530

    Hide similar products

--- application/views/product_details.php
+++ application/views/product_details.php
@@ -34,9 +34,10 @@
 								<img src="images/product-details/1.jpg" alt="" />
 								<!--<h3>ZOOM</h3>-->
 							</div>
+							<!-- Hide similar product carousel
 							<div id="similar-product" class="carousel slide" data-ride="carousel">
 
-								  <!-- Wrapper for slides -->
+								  <!-- Wrapper for slides
 								    <div class="carousel-inner">
 										<div class="item active">
 										  <a href=""><img src="images/product-details/similar1.jpg" alt=""></a>
@@ -53,17 +54,15 @@
 										  <a href=""><img src="images/product-details/similar2.jpg" alt=""></a>
 										  <a href=""><img src="images/product-details/similar3.jpg" alt=""></a>
 										</div>
-
 									</div>
-
-								  <!-- Controls -->
+								  <!-- Controls
 								  <a class="left item-control" href="#similar-product" data-slide="prev">
 									<i class="fa fa-angle-left"></i>
 								  </a>
 								  <a class="right item-control" href="#similar-product" data-slide="next">
 									<i class="fa fa-angle-right"></i>
 								  </a>
-							</div>
+							</div> End hiding-->
 
 						</div>
 						<div class="col-sm-7">
