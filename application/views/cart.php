7b531f68e801eb51230c3bea5405424262446831
nerjerry <psinghal4210@gmail.com>
2015-11-20 01:11:57 +0530

    Display cart Item

---
+++ application/views/cart.php
@@ -0,0 +1,132 @@
+	<section id="cart_items">
+		<div class="container">
+			<div class="breadcrumbs">
+				<ol class="breadcrumb">
+				  <li><a href="#">Home</a></li>
+				  <li class="active">Shopping Cart</li>
+				</ol>
+			</div>
+			<div class="table-responsive cart_info">
+				<table class="table table-condensed">
+					<thead>
+						<tr class="cart_menu">
+							<td class="image">Item</td>
+							<td class="description"></td>
+							<td class="price">Price</td>
+							<td class="quantity">Quantity</td>
+							<td class="total">Total</td>
+							<td></td>
+						</tr>
+					</thead>
+					<tbody>
+						<?php foreach($cart as $cartItem):?>
+						<?php $productId=$cartItem->P_Id;?>
+						<tr>
+							<td class="cart_product">
+								<a href=""><img src="<?php echo base_url($products[$productId]->P_Img)?>" alt=""></a>
+							</td>
+							<td class="cart_description">
+								<h4><a href=""><?php echo $products[$productId]->P_Name;?></a></h4>
+							</td>
+							<td class="cart_price">
+								<p>&#8377;<?php echo $products[$productId]->P_Price;?></p>
+							</td>
+							<td class="cart_quantity">
+								<div class="cart_quantity_button">
+									<!--Use later:<a class="cart_quantity_up" href=""> + </a>-->
+									<input class="cart_quantity_input" type="text" name="quantity" value="<?php echo $cartItem->Quantity;?>" autocomplete="off" size="2">
+									<!-- use later:<a class="cart_quantity_down" href=""> - </a>-->
+								</div>
+							</td>
+							<td class="cart_total">
+								<p class="cart_total_price"><?php echo $cartItem->Amount;?></p>
+							</td>
+							<!--TODO:<td class="cart_delete">
+								<a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
+							</td>-->
+						</tr>
+						<?php endforeach;?>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</section> <!--/#cart_items-->
+
+	<section id="do_action">
+		<div class="container">
+			<div class="heading">
+				<h3>What would you like to do next?</h3>
+				<p>Choose if you have a discount code or reward points you want to use or would like to estimate your delivery cost.</p>
+			</div>
+			<div class="row">
+				<div class="col-sm-6">
+					<div class="chose_area">
+						<ul class="user_option">
+							<li>
+								<input type="checkbox">
+								<label>Use Coupon Code</label>
+							</li>
+							<li>
+								<input type="checkbox">
+								<label>Use Gift Voucher</label>
+							</li>
+							<li>
+								<input type="checkbox">
+								<label>Estimate Shipping & Taxes</label>
+							</li>
+						</ul>
+						<ul class="user_info">
+							<li class="single_field">
+								<label>Country:</label>
+								<select>
+									<option>United States</option>
+									<option>Bangladesh</option>
+									<option>UK</option>
+									<option>India</option>
+									<option>Pakistan</option>
+									<option>Ucrane</option>
+									<option>Canada</option>
+									<option>Dubai</option>
+								</select>
+
+							</li>
+							<li class="single_field">
+								<label>Region / State:</label>
+								<select>
+									<option>Select</option>
+									<option>Dhaka</option>
+									<option>London</option>
+									<option>Dillih</option>
+									<option>Lahore</option>
+									<option>Alaska</option>
+									<option>Canada</option>
+									<option>Dubai</option>
+								</select>
+
+							</li>
+							<li class="single_field zip-field">
+								<label>Zip Code:</label>
+								<input type="text">
+							</li>
+						</ul>
+						<a class="btn btn-default update" href="">Get Quotes</a>
+						<a class="btn btn-default check_out" href="">Continue</a>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="total_area">
+						<ul>
+							<li>Cart Sub Total <span>$59</span></li>
+							<li>Eco Tax <span>$2</span></li>
+							<li>Shipping Cost <span>Free</span></li>
+							<li>Total <span>$61</span></li>
+						</ul>
+							<a class="btn btn-default update" href="">Update</a>
+							<a class="btn btn-default check_out" href="">Check Out</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section><!--/#do_action-->
+
+<?php require_once('templates/footer.php');?>
