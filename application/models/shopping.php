03e5dbb562fb2b42cf6fb0e7ae0274031891fb1f
nerjerry <psinghal4210@gmail.com>
2015-11-20 01:11:24 +0530

    Update item if same item added twice

--- application/models/shopping.php
+++ application/models/shopping.php
@@ -12,18 +12,31 @@ class Shopping extends CI_Model{
 	}
 	public function addItem($data){
 		if($this->checkItem($data)){
-			$this->db->where('Session_Id',$data['Session_Id']);
-			$this->db->where('User_Id',$data['User_Id']);
+			if(isset($_SESSION['logged']))
+				$this->db->where('User_Id',$data['User_Id']);
+			else
+				$this->db->where('Session_Id',$data['Session_Id']);
 			$data['Quantity']+=1;
-			$this->db->update('cart',$data);
+			$data['Amount']=$data['Quantity']*$data['Amount'];
+			$this->db->set('Quantity',$data['Quantity']);
+			$this->db->set('Amount',$data['Amount']);
+			$this->db->where('P_Id',$data['P_Id']);
+			if(isset($_SESSION['logged']))
+				$this->db->where('User_Id',$data['User_Id']);
+			else
+				$this->db->where('Session_Id',$data['Session_Id']);
+			$this->db->update('cart');
 		}else{
 			$this->db->insert('cart',$data);
 		}
 	}
 
 	public function getCart($userId){
-		$this->db->select('P_Id','Quantity','Amount');
-		$this->db('Session_Id',$userId);
+		$this->db->select('P_Id,Quantity,Amount');
+		if(isset($_SESSION['logged']))
+			$this->db->where('User_Id',$userId);
+		else
+			$this->db->where('Session_Id',$userId);
 		$query = $this->db->get('cart');
 		return $query->result();
 	}
