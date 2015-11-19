c11260a902e433f5dea45b64dd21458f0cf2e198
nerjerry <psinghal4210@gmail.com>
2015-11-20 01:11:43 +0530

    Cart Controller:Show Cart

--- application/controllers/cart.php
+++ application/controllers/cart.php
@@ -5,7 +5,13 @@ class Cart extends CI_Controller{
 	public function __construct(){
 		parent::__construct();
 		$this->load->model('shopping');
+		$this->load->model('product');
 		$this->load->helper('cookie');
+	}
+
+	private function getData(&$data){
+		$data['categories']=$this->product->getCategory();
+		$data['brands']=$this->product->getBrand();
 	}
 
 	public function addToCart($product){
@@ -20,15 +26,26 @@ class Cart extends CI_Controller{
 		if(isset($_SESSION['logged']))
 		   $data['User_Id']=$_SESSION['id'];
 		$data['Quantity']=$quantity;
-		$this->load->model('product');
 		$productPrice=$this->product->getProductPrice($product);
 		$data['Amount'] = $quantity*$productPrice;
 		$this->shopping->addItem($data);
 		set_cookie("userId",$data['Session_Id'],60*60*24*3);
-		$this->showCart();
+		redirect('cart/showCart');
 	}
 
 	public function showCart(){
-
+		if(isset($_SESSION['logged'])){
+			$userId = $_SESSION['id'];
+		}else{
+			$userId = get_cookie("userId");
+		}
+		$data['cart'] = $this->shopping->getCart($userId);
+		foreach($data['cart'] as $cartItem){
+			$productId=$cartItem->P_Id;
+			$data['products'][$productId]=$this->product->getProduct($cartItem->P_Id);
+		}
+		$this->getData($data);
+		$this->load->view('templates/header.php',$data);
+		$this->load->view('cart.php',$data);
 	}
 }
