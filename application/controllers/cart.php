ba1da3643a33682160b4938f106e9b49949864fb
nerjerry <psinghal4210@gmail.com>
2015-11-20 00:28:33 +0530

    CART:Add User Id also if user logged in

--- application/controllers/cart.php
+++ application/controllers/cart.php
@@ -10,17 +10,25 @@ class Cart extends CI_Controller{
 
 	public function addToCart($product){
 		$data['Session_Id']=session_id();
+		$data['User_Id']=null;
 		$data['P_Id']=$product;
 		if(null!==$this->input->post('quantity')){
 			$quantity = $this->input->post('quantity');
 		}else{
 			$quantity = 1;
 		}
+		if(isset($_SESSION['logged']))
+		   $data['User_Id']=$_SESSION['id'];
 		$data['Quantity']=$quantity;
 		$this->load->model('product');
 		$productPrice=$this->product->getProductPrice($product);
 		$data['Amount'] = $quantity*$productPrice;
 		$this->shopping->addItem($data);
 		set_cookie("userId",$data['Session_Id'],60*60*24*3);
+		$this->showCart();
+	}
+
+	public function showCart(){
+
 	}
 }
