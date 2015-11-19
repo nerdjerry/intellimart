4023348386c010a3da2cdc79670dc65d7512dcee
nerjerry <psinghal4210@gmail.com>
2015-11-19 14:59:49 +0530

    Display tabs for various categories

--- application/views/index.php
+++ application/views/index.php
@@ -46,15 +46,16 @@
 						</div>
 						<?php endforeach;?>
 					</div><!--features_items-->
-
 					<div class="category-tab"><!--category-tab-->
 						<div class="col-sm-12">
 							<ul class="nav nav-tabs">
-								<li class="active"><a href="#tshirt" data-toggle="tab">T-Shirt</a></li>
-								<li><a href="#blazers" data-toggle="tab">Blazers</a></li>
-								<li><a href="#sunglass" data-toggle="tab">Sunglass</a></li>
-								<li><a href="#kids" data-toggle="tab">Kids</a></li>
-								<li><a href="#poloshirt" data-toggle="tab">Polo shirt</a></li>
+								<?php foreach($categories as $category):?>
+									<?php if($category==$categories[0]):?>
+										<li class="active"><a href="#<?php echo $category->name?>" data-toggle="tab"><?php echo $category->name?></a></li>
+									<?php continue;?>
+									<?php endif;?>
+								<li><a href="#<?php echo $category->name?>" data-toggle="tab"><?php echo $category->name?></a></li>
+								<?php endforeach;?>
 							</ul>
 						</div>
 						<div class="tab-content">
